0c79c5a097b0b75175a91c38bbd945fd5f12674d
danilsyah <danilsyaharihardjo@gmail.com>
2022-11-27 01:40:50 +0700

    update message response

--- app/Http/Controllers/FootballerController.php
+++ app/Http/Controllers/FootballerController.php
@@ -9,7 +9,7 @@ class FootballerController extends Controller
 {
     public function index(){
         $footballers = Footballer::get();
-        return response()->json(["success" => true,"message" => "Success", "data" => $footballers], 200);
+        return response()->json(["success" => true,"message" => "Footballer all found", "data" => $footballers], 200);
     }
 
     public function show($id = null){
@@ -18,7 +18,7 @@ class FootballerController extends Controller
 
         if($footballer){
             // $footballer = $footballer->toJson(JSON_PRETTY_PRINT);
-            return response()->json(["success" => true,"message" => "Success", "data" => $footballer], 200);
+            return response()->json(["success" => true,"message" => "Footballer found", "data" => $footballer], 200);
         }else{
             return response()->json(["success"=>false,"message"=>"Footballer not found"], 404);
         }
